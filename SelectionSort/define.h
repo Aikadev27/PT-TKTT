84ef0d017a8415433dd03a17e67caad563367fe9
Aikadev <aikadev27@gmail.com>
2022-04-04 23:44:58 +0700

    sap xep chon

---
+++ SelectionSort/define.h
@@ -0,0 +1,30 @@
+
+typedef int keytype;
+typedef float othertype;
+
+typedef struct
+{
+    keytype key;
+    othertype otherfield;
+} recordtype;
+
+void swap(recordtype *x, recordtype *y)
+{
+    recordtype temp;
+    temp = *x;
+    *x = *y;
+    *y =temp;
+    
+}
+ void input(int a[], int n){
+     scanf("%d", &n);
+     for(int i=0; i<n; i++){
+         scanf("%d", &a[i]);
+     }
+ }
+ void output(int a[], int n){
+      for(int i=0; i<n; i++){
+         printf("%5d", a[i]);
+         printf("\n");
+     }
+ }
---
+++ SelectionSort/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "define.h"
+
+void SelectionSort(recordtype a[], int n)
+{
+
+    int i, j, lowindex;
+    keytype lowkey;
+    for (i = 0; i <= n - 2; i++)
+    {
+        lowkey = a[i].key;
+        lowindex = i;
+        for (j = i + 1; j <= n - 1; j++)
+        {
+            if (a[j].key < lowkey)
+                lowkey = a[j].key;
+            lowindex = j;
+        }
+        swap(&a[i], &a[lowindex]);
+    }
+}
+// int main()
+// {
+//     int a[20], n;
+//     input(a,n);
+    
+//     printf(" before sort\n");
+//     output(a,n);
+//     printf("after sort\n");
+//     SelectionSort(a,&n);
+//     output(a,n);
+//     return 0;
+// }
